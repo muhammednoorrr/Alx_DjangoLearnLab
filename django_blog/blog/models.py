8c5a7d07aba5c1eca0a2f55cc1cb757c65edbd98
Muhammed Ahmed <muhamme7d7@gmail.com>
2025-10-04 10:10:24 -0900

    Update models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -5,6 +5,7 @@ from taggit.managers import TaggableManager
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True) 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
